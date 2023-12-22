dfd5ec51e9b2301973bd6868e3bfd7058020bda9
Kosuke Takada <tako@KosukenoMacBook-Air.local>
2023-12-22 16:11:20 +0900

    add packages

--- scripts/ArchLinux/pacman.sh
+++ scripts/ArchLinux/pacman.sh
@@ -2,7 +2,7 @@
 
 PACKAGES=" base base-devel zsh git vim openssh curl wget systemd"
 PACKAGES+=" zsh-completions zsh-syntax-highlighting"
-PACKAGES+=" pwgen tree jq yq tcpdump sed gzip tar whois"
+PACKAGES+=" pwgen tree jq yq tcpdump sed gzip tar whois neofetch"
 PACKAGES+=" docker kubectl helm minikube"
 PACKAGES+=" go"
 
