6adb3d90faa904eeb2a24479f381145e4421e0fa
tako919191 <ef.phv2.m@gmail.com>
2023-12-22 13:21:52 +0900

    update

--- bin/ArchLinux/makeSymliksForArch.sh
+++ bin/ArchLinux/makeSymliksForArch.sh
@@ -1,11 +1,11 @@
 #!/bin/sh
-mkdir -p ~/.config/i3
-mkdir -p ~/.config/i3blocks
-mkdir -p ~/.config/termite
-mkdir -p ~/.config/polybar
+# mkdir -p ~/.config/i3
+# mkdir -p ~/.config/i3blocks
+# mkdir -p ~/.config/termite
+# mkdir -p ~/.config/polybar
 
-ln -sf ~/dotfiles/_Xresources ~/.Xresources
-ln -sf ~/dotfiles/.config/i3/config ~/.config/i3/config
-ln -sf ~/dotfiles/.config/i3blocks/config ~/.config/i3blocks/config
-ln -sf ~/dotfiles/.config/polybar/config ~/.config/polybar/config
-ln -sf ~/dotfiles/.config/termite/config ~/.config/termite/config
+# ln -sf ~/dotfiles/_Xresources ~/.Xresources
+# ln -sf ~/dotfiles/.config/i3/config ~/.config/i3/config
+# ln -sf ~/dotfiles/.config/i3blocks/config ~/.config/i3blocks/config
+# ln -sf ~/dotfiles/.config/polybar/config ~/.config/polybar/config
+# ln -sf ~/dotfiles/.config/termite/config ~/.config/termite/config
